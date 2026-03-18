2b0c6c3961aac4ad90e504968c150abf541ef06f
devidutta-07 <deviduttame32@gmail.com>
2026-03-18 20:59:48 +0530

    Pydentic Object

--- ChatModels/StructureOutput/Pydentic.py
+++ ChatModels/StructureOutput/Pydentic.py
@@ -1,10 +1,35 @@
 from langchain_mistralai import ChatMistralAI
-from langchain_core.prompts import ChatMessagePromptTemplate
-
+from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+from typing import List, Optional
+from langchain_core.output_parsers import PydanticOutputParser
 from dotenv import load_dotenv
 load_dotenv()
 
 model=ChatMistralAI(
     model_name="mistral-small-latest",
 )
+class format (BaseModel):
+    titel:str
+    genre:List[str]
+    director:Optional[str]
+    rating:Optional[float]  
 
+parser=PydanticOutputParser(pydantic_object=format)
+
+prompt=ChatPromptTemplate.from_messages([
+    ("system","You are a Movie Details Extractor . Extract movie deatils like this {format}"),
+    ("human","Extract Movie Details : {movie_name}")
+]
+)
+movie=input("Enter Movie Name :")
+
+final_prompt=prompt.invoke({
+    "movie_name":movie,
+    "format": parser.get_format_instructions()
+})
+
+response=model.invoke(final_prompt)
+
+print(final_prompt)
+print(response.content)
