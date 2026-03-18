fdb5ae6f500147e22a510545759f1a62526ef1ad
devidutta-07 <deviduttame32@gmail.com>
2026-03-18 20:08:01 +0530

    MovieExtractor Detail Project

---
+++ ChatModels/Applications/MovieExtractor/app.py
@@ -0,0 +1,41 @@
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatMistralAI(
+    model_name="mistral-small-latest"
+)
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", """You are a movie information assistant.
+
+The user will provide a movie name. Your task is to extract and return detailed information about the movie in strictly valid JSON format.
+
+Include the following fields:
+- title
+- genre (as a list)
+- release_year
+- director
+- cast (as a list of main actors)
+- language
+- country
+- plot_summary (short, 2-3 lines)
+- imdb_rating (if available)
+
+Rules:
+- Return ONLY JSON. No explanation, no extra text.
+- If any information is not available, use null.
+- Ensure the JSON is properly formatted and valid.
+"""),
+    
+    ("human", "Movie name: {movie_name}")
+])
+
+movie = input("Enter Movie Name: ")
+
+final_prompt = prompt.invoke({"movie_name": movie})
+result = model.invoke(final_prompt)
+
+print(result.content)
